eced5d762ef97e1313b10e92031bbfc2a3a4a79d
matamorosjose <jmatamorossantos@my.wctc.edu>
2021-09-23 18:21:20 -0500

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace In_Class_Abstract_Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
